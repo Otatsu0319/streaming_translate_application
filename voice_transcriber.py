0796881d83ce1d530244b6b83c660780d1bdcab4
Otatsu0319 <91446491+Otatsu0319@users.noreply.github.com>
2024-10-06 15:01:43 +0000

    翻訳部を別プロセス化

--- voice_transcriber.py
+++ voice_transcriber.py
@@ -1,3 +1,5 @@
+import multiprocessing as mp
+
 import numpy as np
 from faster_whisper import WhisperModel
 from llama_cpp import Llama
@@ -8,6 +10,28 @@ PROMPT_JP2EN = "Translate this from Japanese to English:\nJapanese: {0}\nEnglish
 PROMPT_EN2JP = "Translate this from English to Japanese:\nEnglish: {0}\nJapanese:"
 
 
+mp.set_start_method('spawn', force=True)
+
+
+def translate_process(queue):
+    translator = Llama.from_pretrained(
+        "mmnga/webbigdata-ALMA-7B-Ja-V2-gguf",
+        "*q4_0.gguf",
+        local_dir="/workspace/models",
+        cache_dir="/workspace/models",
+        n_gpu_layers=-1,
+        verbose=False,
+    )  # VRAM 6.7GB?
+
+    while True:
+        segment = queue.get()
+        if segment is None:
+            break
+        print("[id:%d, p:%.02f] %s" % (segment.id, segment.avg_logprob, segment.text))
+        output = translator(PROMPT_EN2JP.format(segment.text), max_tokens=128, stop=["\n"])
+        print("translated: ", output['choices'][0]['text'])
+
+
 class VoiceTranscriber:
     def __init__(self, speech_queue, translate=True):
         self.speech_queue = speech_queue
@@ -16,20 +40,17 @@ class VoiceTranscriber:
         self.transcribe_log_probability_threshold = -0.4
 
         self.translate = translate
+        self.text_queue = mp.Queue()
         if translate:
-            self.translator = Llama.from_pretrained(
-                "mmnga/webbigdata-ALMA-7B-Ja-V2-gguf",
-                "*q4_0.gguf",
-                local_dir="/workspace/models",
-                cache_dir="/workspace/models",
-                n_gpu_layers=-1,
-                verbose=False,
-            )  # VRAM 6.7GB?
+            self.p = mp.Process(target=translate_process, args=(self.text_queue,))
+            self.p.start()
 
     def transcribe_thread(self):
         while True:
             speech = self.speech_queue.get()
             if speech is None:
+                self.text_queue.put(None)
+                self.p.join()
                 break
             speech = np.array(speech)
 
@@ -37,8 +58,8 @@ class VoiceTranscriber:
                 speech, language="en", log_prob_threshold=self.transcribe_log_probability_threshold
             )
             for segment in segments:
-                print("[id:%d, p:%.02f] %s" % (segment.id, segment.avg_logprob, segment.text))
 
                 if self.translate:
-                    output = self.translator(PROMPT_EN2JP.format(segment.text), max_tokens=128, stop=["\n"])
-                    print("translated: ", output['choices'][0]['text'])
+                    self.text_queue.put(segment)
+                else:
+                    print("[id:%d, p:%.02f] %s" % (segment.id, segment.avg_logprob, segment.text))

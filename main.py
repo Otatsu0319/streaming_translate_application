abce02546e0a80789fdd90bd5bafe9505bf2589d
Otatsu0319 <91446491+Otatsu0319@users.noreply.github.com>
2024-10-06 14:19:32 +0000

    mainをスレッド化し終了しやすく

--- main.py
+++ main.py
@@ -19,96 +19,99 @@ SAMPLING_RATE = 16000
 CHUNK_SIZE = 512  # silero_vad model's chunk size
 
 
-def main():
-    logging.basicConfig(level=logging.ERROR)
+class StreamingTranslateApp:
+    def __init__(self):
+        logging.basicConfig(level=logging.ERROR)
 
-    vs = voice_separator.VoiceSeparator()
+        self.vs = voice_separator.VoiceSeparator()
 
-    receiver = streamer.VBANStreamingReceiver(
-        "127.0.0.1",
-        "Stream1",
-        6981,
-        current_sample_rate=vs.model_samplerate,
-        current_channels=vs.model_audio_channels,
-        chunk_size=vs.chunk_size - vs.overlap_frames,
-    )
-    # receiver = streamer.WavStreamReceiver(
-    #     "./audio_samples/sample_elira.wav",
-    #     current_sample_rate=vs.model_samplerate,
-    #     current_channels=vs.model_audio_channels,
-    #     chunk_size=vs.chunk_size - vs.overlap_frames,
-    # )
-    sound_queue = queue.Queue()
-    speech_queue = queue.Queue()
+        self.receiver = streamer.VBANStreamingReceiver(
+            "127.0.0.1",
+            "Stream1",
+            6981,
+            current_sample_rate=self.vs.model_samplerate,
+            current_channels=self.vs.model_audio_channels,
+            chunk_size=self.vs.chunk_size - self.vs.overlap_frames,
+        )
+        self.sound_queue = queue.Queue()
+        self.speech_queue = queue.Queue()
 
-    transcriber = voice_transcriber.VoiceTranscriber(speech_queue)
+        self.transcriber = voice_transcriber.VoiceTranscriber(self.speech_queue)
 
-    def sound_receiv_thread():
-        for chunk in receiver.recv_generator():
-            sound_queue.put(chunk.T)
+        self.silero_model = load_silero_vad(onnx=True)
+        self.vad_threshold = 0.5
+        # It is better to tune this parameter for each dataset separately,
+        # but "lazy" 0.5 is pretty good for most datasets.
 
-    silero_model = load_silero_vad(onnx=True)
-    vad_threshold = 0.5
-    # It is better to tune this parameter for each dataset separately,
-    # but "lazy" 0.5 is pretty good for most datasets.
-    onvoice_timeout = 0.5  # sec
-    onvoice_timeout_chunk_count = (
-        int(onvoice_timeout * SAMPLING_RATE) // CHUNK_SIZE + 0
-        if int(onvoice_timeout * SAMPLING_RATE) % CHUNK_SIZE == 0
-        else 1
-    )
-    buffer = []
+        onvoice_timeout = 0.5  # sec
+        self.onvoice_timeout_chunk_count = (
+            int(onvoice_timeout * SAMPLING_RATE) // CHUNK_SIZE + 0
+            if int(onvoice_timeout * SAMPLING_RATE) % CHUNK_SIZE == 0
+            else 1
+        )
 
-    print("stand-by cmpl.")
+        self.buffer = []
+        self.chunk_buffer = np.zeros(0)
 
-    th = Thread(target=transcriber.transcribe_thread)
-    th.start()
-    sd_th = Thread(target=sound_receiv_thread)
-    sd_th.start()
+        self.tr_th = Thread(target=self.transcriber.transcribe_thread)
+        self.sd_th = Thread(target=self.sound_receiv_thread)
+        print("stand-by cmpl.")
 
-    print("start streaming...")
+    def sound_receiv_thread(self):
+        for chunk in self.receiver.recv_generator():
+            self.sound_queue.put(chunk.T)
 
-    chunk_buffer = np.zeros(0)
+    def streaming_thread(self):
+        self.tr_th.start()
+        self.sd_th.start()
 
-    timeout_counter = 0
+        timeout_counter = 0
 
-    start_time = time.time()
+        print("start streaming...")
 
-    for chunk in vs.extract_streamer(sound_queue):
-        chunk = librosa.to_mono(chunk)
-        chunk = librosa.resample(chunk, orig_sr=vs.model_samplerate, target_sr=SAMPLING_RATE)
+        for chunk in self.vs.extract_streamer(self.sound_queue):
+            chunk = librosa.to_mono(chunk)
+            chunk = librosa.resample(chunk, orig_sr=self.vs.model_samplerate, target_sr=SAMPLING_RATE)
 
-        chunk_buffer = np.append(chunk_buffer, chunk)
-        i = 0
-        for i in range(0, len(chunk_buffer) - CHUNK_SIZE, CHUNK_SIZE):
-            chunk = torch.Tensor(chunk_buffer[i : i + CHUNK_SIZE]).float()
+            self.chunk_buffer = np.append(self.chunk_buffer, chunk)
 
-            speech_prob = silero_model(chunk, SAMPLING_RATE).item()
-            # print(speech_prob)
+            for i in range(0, len(self.chunk_buffer) - CHUNK_SIZE, CHUNK_SIZE):
+                chunk = torch.Tensor(self.chunk_buffer[i : i + CHUNK_SIZE]).float()
 
-            if vad_threshold < speech_prob:
-                timeout_counter = 0
-            else:
-                timeout_counter += 1
+                speech_prob = self.silero_model(chunk, SAMPLING_RATE).item()
 
-            if vad_threshold < speech_prob and timeout_counter <= onvoice_timeout_chunk_count:
-                buffer.extend(chunk.tolist())
+                if self.vad_threshold < speech_prob:
+                    timeout_counter = 0
+                else:
+                    timeout_counter += 1
 
-            if speech_prob < vad_threshold and onvoice_timeout_chunk_count < timeout_counter and 0 < len(buffer):
-                transcriber.speech_queue.put(buffer)
-                buffer = []
+                if self.vad_threshold < speech_prob and timeout_counter <= self.onvoice_timeout_chunk_count:
+                    self.buffer.extend(chunk.tolist())
 
-        chunk_buffer = chunk_buffer[i + CHUNK_SIZE :]
-        if start_time + 10 < time.time():
-            print("ending streaming...")
-            sound_queue.put(None)
-            speech_queue.put(None)
-            receiver._running = False
-            break
+                if (
+                    speech_prob < self.vad_threshold
+                    and self.onvoice_timeout_chunk_count < timeout_counter
+                    and 0 < len(self.buffer)
+                ):
+                    self.transcriber.speech_queue.put(self.buffer)
+                    self.buffer = []
 
-    th.join()
-    sd_th.join()
+            self.chunk_buffer = self.chunk_buffer[i + CHUNK_SIZE :]
+
+        self.tr_th.join()
+        self.sd_th.join()
+
+    def end(self):
+        print("shutting down...")
+        self.sound_queue.put(None)
+        self.speech_queue.put(None)
+        self.receiver._running = False
 
 
 if __name__ == "__main__":
-    main()
+    stapp = StreamingTranslateApp()
+    th = Thread(target=stapp.streaming_thread)
+    th.start()
+
+    input("Press Enter to stop...\n")
+    stapp.end()

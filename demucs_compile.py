6dc9d6b0b35807018a2d6bbb6d86ef844a9b35cf
Otatsu0319 <91446491+Otatsu0319@users.noreply.github.com>
2024-10-06 12:10:34 +0000

    code formatted

--- demucs_compile.py
+++ demucs_compile.py
@@ -1,18 +1,19 @@
 # 音声分離を行うdemucsモデルをパッケージをインポートせずに使えるように、jitコンパイルを行う
 # Jit compile so that you can use the demucs model for speech separation without importing the package
-# 
+#
 # 使用モデル(using models): hybrid_transformer (Demucs v4)
 
-import torch
 import inspect
 import warnings
+
+import torch
 from demucs import states
 
 URL = "https://dl.fbaipublicfiles.com/demucs/hybrid_transformer/955717e8-8726e21a.th"
 
 
-def get_model(url = URL):
-    pkg = torch.hub.load_state_dict_from_url(url, model_dir="./demucs_models", map_location='cpu', check_hash=True) 
+def get_model(url=URL):
+    pkg = torch.hub.load_state_dict_from_url(url, model_dir="./demucs_models", map_location='cpu', check_hash=True)
 
     klass = pkg["klass"]
     args = pkg["args"]
@@ -27,7 +28,7 @@ def get_model(url = URL):
             del kwargs[key]
     model = klass(*args, **kwargs)
     states.set_state(model, state)
-    
+
     return model
 
 
@@ -35,16 +36,18 @@ if __name__ == "__main__":
     model = get_model()
     model.to("cuda")
     model.eval()
-    
+
     segment = model.segment
     segment_length: int = int(model.samplerate * segment)
-    example_tensor = torch.rand((1, model.audio_channels, segment_length), device="cuda") # (1, 2, 343980) # Batch, Channels, Times(audio signal)
-  
+    example_tensor = torch.rand(
+        (1, model.audio_channels, segment_length), device="cuda"
+    )  # (1, 2, 343980) # Batch, Channels, Times(audio signal)
+
     with torch.no_grad():
         torch.jit.trace(model, example_inputs=example_tensor, check_trace=False).save("./demucs_models/demucs_ts.pt")
 
     # get model info (need to inference)
     print(f"model_samplerate={model.samplerate}")
     print(f"model_audio_channels={model.audio_channels}")
-    print(f"model_souces={model.sources}") 
+    print(f"model_souces={model.sources}")
     print(f"model_segment={model.segment}")

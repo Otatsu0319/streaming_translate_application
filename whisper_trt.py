733370ff757d21db2c599c18dee7861972226e52
Otatsu0319 <91446491+Otatsu0319@users.noreply.github.com>
2024-10-16 18:51:41 +0000

    クラス内にtranscribe機能を内包

--- whisper_trt.py
+++ whisper_trt.py
@@ -28,12 +28,13 @@ import numpy as np
 import soundfile
 import torch
 import torch.nn.functional as F
-from whisper.audio import HOP_LENGTH, N_FFT, N_SAMPLES, SAMPLE_RATE  # , CHUNK_LENGTH
+from whisper.audio import HOP_LENGTH, N_FFT, N_SAMPLES  # , SAMPLE_RATE, CHUNK_LENGTH
 from whisper.normalizers import EnglishTextNormalizer
 
 os.environ['TLLM_LOG_LEVEL'] = 'ERROR'
 
 if os.environ['TLLM_LOG_LEVEL'] in ["INTERNAL_ERROR", "ERROR", "WARNING", "INFO", "VERBOSE", "DEBUG"]:
+    from tensorrt_llm import logger
     from tensorrt_llm._utils import str_dtype_to_torch
     from tensorrt_llm.bindings import GptJsonConfig
     from tensorrt_llm.runtime import ModelRunnerCpp
@@ -74,67 +75,6 @@ def pad_or_trim(array, length: int = N_SAMPLES, *, axis: int = -1):
     return array
 
 
-def log_mel_spectrogram(
-    audio: Union[str, np.ndarray, torch.Tensor],
-    n_mels: int,
-    padding: int = 0,
-    device: Optional[Union[str, torch.device]] = None,
-    return_duration: bool = False,
-    mel_filters_dir: str = None,
-):
-    """
-    Compute the log-Mel spectrogram of
-
-    Parameters
-    ----------
-    audio: Union[str, np.ndarray, torch.Tensor], shape = (*)
-        The path to audio or either a NumPy array or Tensor containing the audio waveform in 16 kHz
-
-    n_mels: int
-        The number of Mel-frequency filters, only 80 and 128 are supported
-
-    padding: int
-        Number of zero samples to pad to the right
-
-    device: Optional[Union[str, torch.device]]
-        If given, the audio tensor is moved to this device before STFT
-
-    Returns
-    -------
-    torch.Tensor, shape = (80 or 128, n_frames)
-        A Tensor that contains the Mel spectrogram
-    """
-    if not torch.is_tensor(audio):
-        if isinstance(audio, str):
-            audio, _ = load_audio_wav_format(audio)
-
-        assert isinstance(audio, np.ndarray), f"Unsupported audio type: {type(audio)}"
-        duration = audio.shape[-1] / SAMPLE_RATE
-        audio = pad_or_trim(audio, N_SAMPLES)
-        audio = audio.astype(np.float32)
-        audio = torch.from_numpy(audio)
-
-    if device is not None:
-        audio = audio.to(device)
-    if padding > 0:
-        audio = F.pad(audio, (0, padding))
-    window = torch.hann_window(N_FFT).to(audio.device)
-    stft = torch.stft(audio, N_FFT, HOP_LENGTH, window=window, return_complex=True)
-    magnitudes = stft[..., :-1].abs() ** 2
-
-    mel_128 = librosa.filters.mel(sr=16000, n_fft=400, n_mels=128)
-    filters = torch.from_numpy(mel_128).to(device)
-    mel_spec = filters @ magnitudes
-
-    log_spec = torch.clamp(mel_spec, min=1e-10).log10()
-    log_spec = torch.maximum(log_spec, log_spec.max() - 8.0)
-    log_spec = (log_spec + 4.0) / 4.0
-    if return_duration:
-        return log_spec, duration
-    else:
-        return log_spec
-
-
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('--log_level', type=str, default='warning')
@@ -153,23 +93,15 @@ def parse_arguments():
     return parser.parse_args()
 
 
-def read_config(component, engine_dir):
-    config_path = engine_dir / component / 'config.json'
-    with open(config_path, 'r') as f:
-        config = json.load(f)
-    model_config = OrderedDict()
-    model_config.update(config['pretrained_config'])
-    model_config.update(config['build_config'])
-    return model_config
-
-
 class WhisperTRTLLM(object):
-    def __init__(self, engine_dir, debug_mode=False, use_py_session=False):
+    def __init__(self, engine_dir, debug_mode=False, device="cuda"):
         engine_dir = Path(engine_dir)
-        encoder_config = read_config('encoder', engine_dir)
-        decoder_config = read_config('decoder', engine_dir)
+        encoder_config = self.read_config('encoder', engine_dir)
+        decoder_config = self.read_config('decoder', engine_dir)
         self.n_mels = encoder_config['n_mels']
         self.num_languages = encoder_config['num_languages']
+        self.batch_size = 1
+
         is_multilingual = decoder_config['vocab_size'] >= 51865
         if is_multilingual:
             tokenizer_name = "multilingual"
@@ -177,6 +109,14 @@ class WhisperTRTLLM(object):
             tokenizer_name = "gpt2"
         self.tokenizer = tokenizer.get_encoding(tokenizer_name, self.num_languages)
         self.eot_id = self.tokenizer.encode("<|endoftext|>", allowed_special=self.tokenizer.special_tokens_set)[0]
+
+        self.normalizer = EnglishTextNormalizer()
+
+        self.device = device
+        self.mel_filter = torch.from_numpy(
+            librosa.filters.mel(sr=16000, n_fft=400, n_mels=self.n_mels, dtype=np.float64)
+        ).to(self.device)
+        self.window = torch.hann_window(N_FFT).to(self.device)
 
         json_config = GptJsonConfig.parse_file(engine_dir / 'decoder' / 'config.json')
         assert json_config.model_config.supports_inflight_batching
@@ -186,21 +126,78 @@ class WhisperTRTLLM(object):
             max_batch_size=16,
             max_input_len=3000,
             max_output_len=96,
-            max_beam_width=4,
+            max_beam_width=1,
             debug_mode=debug_mode,
-            kv_cache_free_gpu_memory_fraction=0.9,
+            kv_cache_free_gpu_memory_fraction=0.1,
         )
         self.model_runner_cpp = ModelRunnerCpp.from_dir(**runner_kwargs)
-        self.use_py_session = use_py_session
 
-    def process_batch(
+    def read_config(self, component, engine_dir):
+        config_path = engine_dir / component / 'config.json'
+        with open(config_path, 'r') as f:
+            config = json.load(f)
+        model_config = OrderedDict()
+        model_config.update(config['pretrained_config'])
+        model_config.update(config['build_config'])
+        return model_config
+
+    # @torch.jit.script
+    def log_mel_spectrogram(
         self,
-        mel,
-        mel_input_lengths,
-        text_prefix="<|startoftranscript|><|en|><|transcribe|><|notimestamps|>",
-        num_beams=1,
-        max_new_tokens=96,
-    ):
+        audio: torch.Tensor,
+    ) -> torch.Tensor:
+        """
+        Compute the log-Mel spectrogram
+
+        Parameters
+        ----------
+        audio: torch.Tensor, shape = (audio_length,)
+            NumPy array containing the audio waveform in 16 kHz
+
+        padding: int
+            Number of zero samples to pad to the right
+
+        device: Optional[Union[str, torch.device]]
+            If given, the audio tensor is moved to this device before STFT
+
+        Returns
+        -------
+        torch.Tensor, shape = (80 or 128, n_frames)
+            A Tensor that contains the Mel spectrogram
+        """
+
+        if audio.shape[-1] > N_SAMPLES:
+            audio = audio.index_select(dim=-1, index=torch.arange(N_SAMPLES, device=audio.device))
+            logger.warning(f"Audio length exceeds; input length: {audio.shape[-1]}, truncating to {N_SAMPLES}")
+        elif audio.shape[-1] < N_SAMPLES:
+            pad_widths = [(0, 0)] * audio.ndim
+            pad_widths[-1] = (0, N_SAMPLES - audio.shape[-1])
+            audio = F.pad(audio, [pad for sizes in pad_widths[::-1] for pad in sizes])
+
+        stft = torch.stft(audio, N_FFT, HOP_LENGTH, window=self.window, return_complex=True)
+        magnitudes = stft[..., :-1].abs() ** 2
+        print(self.mel_filter.type(), magnitudes.type())
+        mel_spec = self.mel_filter @ magnitudes
+
+        log_spec = torch.clamp(mel_spec, min=1e-10).log10()
+        log_spec = torch.maximum(log_spec, log_spec.max() - 8.0)
+        log_spec = (log_spec + 4.0) / 4.0
+
+        return log_spec
+
+    def transcribe(self, audio: np.ndarray, language="en", log_prob_threshold=-float("inf")):
+        audio = torch.from_numpy(audio).to(self.device)
+        mel = self.log_mel_spectrogram(audio)
+        mel = mel.type(str_dtype_to_torch("float16"))
+        mel = mel.unsqueeze(0)
+        mel = mel.repeat(self.batch_size, 1, 1)
+        feature_input_lengths = torch.full((mel.shape[0],), mel.shape[2], dtype=torch.int32, device=mel.device)
+
+        mel_input_lengths = feature_input_lengths
+        text_prefix = "<|startoftranscript|><|en|><|transcribe|><|notimestamps|>"
+        num_beams = 1
+        max_new_tokens = 96
+
         prompt_id = self.tokenizer.encode(text_prefix, allowed_special=self.tokenizer.special_tokens_set)
         prompt_id = torch.tensor(prompt_id)
         batch_size = mel.shape[0]
@@ -215,69 +212,35 @@ class WhisperTRTLLM(object):
                 end_id=self.eot_id,
                 pad_id=self.eot_id,
                 num_beams=num_beams,
-                output_sequence_lengths=True,
+                output_cum_log_probs=True,
+                output_log_probs=True,
                 return_dict=True,
             )
             torch.cuda.synchronize()
             output_ids = outputs['output_ids'].cpu().numpy().tolist()
-        texts = []
+        for key in outputs:
+            print(key, outputs[key].shape)
+            print(outputs[key].cpu().numpy().tolist())
+        predictions = []
         for i in range(len(output_ids)):
             text = self.tokenizer.decode(output_ids[i][0]).strip()
-            texts.append(text)
-        return texts
-
-
-def decode_wav_file(
-    input_file_path,
-    model,
-    text_prefix="<|startoftranscript|><|en|><|transcribe|><|notimestamps|>",
-    dtype='float16',
-    batch_size=1,
-    num_beams=1,
-    normalizer=None,
-    mel_filters_dir=None,
-):
-    mel, total_duration = log_mel_spectrogram(
-        input_file_path, model.n_mels, device='cuda', return_duration=True, mel_filters_dir=mel_filters_dir
-    )
-    mel = mel.type(str_dtype_to_torch(dtype))
-    mel = mel.unsqueeze(0)
-    # repeat the mel spectrogram to match the batch size
-    mel = mel.repeat(batch_size, 1, 1)
-    # TODO: use the actual input_lengths rather than padded input_lengths
-    feature_input_lengths = torch.full((mel.shape[0],), mel.shape[2], dtype=torch.int32, device=mel.device)
-    predictions = model.process_batch(mel, feature_input_lengths, text_prefix, num_beams)
-    prediction = predictions[0]
-
-    # remove all special tokens in the prediction
-    prediction = re.sub(r'<\|.*?\|>', '', prediction)
-    if normalizer:
-        prediction = normalizer(prediction)
-    print(f"prediction: {prediction}")
-    results = [(0, [""], prediction.split())]
-    return results, total_duration
+            text = re.sub(r'<\|.*?\|>', '', text)
+            if self.normalizer:
+                text = self.normalizer(text)
+            predictions.append(text)
+        return predictions
 
 
 if __name__ == '__main__':
     args = parse_arguments()
-    model = WhisperTRTLLM("/mnt/wsl/workspace/TensorRT-LLM/examples/whisper/whisper_large_v3_trt", use_py_session=True)
-    normalizer = EnglishTextNormalizer()
-
+    model = WhisperTRTLLM("/mnt/wsl/workspace/streaming_translate_application/models/whisper_trt_engine")
+    wav, sr = load_audio_wav_format("/mnt/wsl/workspace/streaming_translate_application/results/htdemucs_vocal_16k.wav")
     start_time = time.time()
-    results, total_duration = decode_wav_file(
-        "/mnt/wsl/workspace/streaming_translate_application/results/htdemucs_vocal_16k.wav",
-        model,
-        dtype=args.dtype,
-        batch_size=args.batch_size,
-        num_beams=args.num_beams,
-        mel_filters_dir=args.assets_dir,
-        normalizer=normalizer,
-    )
+    results = model.transcribe(wav, language="en", log_prob_threshold=-float("inf"))
     elapsed = time.time() - start_time
-    results = sorted(results)
 
     print(results)
-
+    total_duration = len(wav) / sr
     rtf = elapsed / total_duration
     s = f"RTF: {rtf:.4f}\n"
     s += f"total_duration: {total_duration:.3f} seconds\n"

c77a9ad001116d27cdc26cb7b78043a5791c1a91
Otatsu0319 <91446491+Otatsu0319@users.noreply.github.com>
2025-01-09 01:46:08 +0000

    デバッグ用の送信器を追加

--- streamer.py
+++ streamer.py
@@ -1,4 +1,5 @@
 import logging
+import queue
 import socket
 
 import librosa
@@ -48,7 +49,7 @@ class VBANStreamingReceiver(StreamReceiver):
 
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self._socket.bind(("0.0.0.0", port))
+        self._socket.bind((sender_ip, port))
 
         self._buff = []
         self._first_check = False
@@ -98,13 +99,14 @@ class VBANStreamingReceiver(StreamReceiver):
         while self._running:
             try:
                 data = self.recv_once()
+
+                data = np.frombuffer(data, dtype="int16")
+                data = data / 32768.0
+                self._buff.extend(data.tolist())
+
             except Exception as e:
                 logging.error(f"An exception occurred: {e}")
                 continue
-
-            data = np.frombuffer(data, dtype="int16")
-            data = data / 32768.0
-            self._buff.extend(data.tolist())
 
             if len(self._buff) >= self.chunk_size:
                 data = self._buff[: self.chunk_size]
@@ -114,6 +116,77 @@ class VBANStreamingReceiver(StreamReceiver):
                     yield np.array([data, data]).T
                 else:
                     yield np.array(data)
+
+
+class VBANStreamingSender:
+    def __init__(
+        self,
+        receiver_ip: str,
+        stream_name: str,
+        port: int,
+        data_queue: queue.Queue,
+        sample_rate: int = 16000,
+        channels: int = 1,
+        logger: logging.Logger = None,
+    ):
+        self._logger = logger if logger else logging.getLogger(f"VBAN_Sender_{receiver_ip}_{port}_{stream_name}")
+
+        self._receiver_ip = receiver_ip
+        self._port = port
+        self._stream_name = stream_name
+        self._sample_rate = sample_rate
+        self._vban_sample_rate = pyvban_audio.const.VBANSampleRatesSR2Enum[sample_rate]
+        self._channels = channels
+
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        self._samples_per_frame = 206
+        self._frame_counter = 0
+        self._buff = []
+
+        self.data_queue = data_queue
+
+    def send_once(self, data: np.ndarray):
+        try:
+
+            header = pyvban.subprotocols.audio.VBANAudioHeader(
+                sample_rate=self._vban_sample_rate,
+                channels=self._channels,
+                samples_per_frame=self._samples_per_frame,
+                format=pyvban_audio.const.VBANBitResolution.VBAN_BITFMT_16_INT,
+                codec=pyvban_audio.const.VBANCodec.VBAN_CODEC_PCM,
+                stream_name=self._stream_name,
+                frame_counter=self._frame_counter,
+            )
+
+            if len(data) != self._samples_per_frame:
+                raise ValueError("Data size is not correct")
+
+            packet = header.to_bytes() + data.tobytes()
+
+            if len(packet) > pyvban.const.VBAN_PROTOCOL_MAX_SIZE:
+                raise ValueError("Packet size is too large")
+
+            self._frame_counter += 1
+            self._socket.sendto(packet, (self._receiver_ip, self._port))
+            self._logger.debug(f"Sent {len(data)} samples")
+
+        except Exception as e:
+            self._logger.error(f"An exception occurred: {e}")
+            raise e
+
+    def send_thread(self):
+        while True:
+            data = self.data_queue.get()
+            if data is None:
+                break
+            data = data * 32768.0
+            self._buff.extend(data.tolist())
+            for _ in range(len(self._buff) // self._samples_per_frame - 1):
+                data = self._buff[: self._samples_per_frame]
+                self._buff = self._buff[self._samples_per_frame :]
+                self.send_once(np.array(data, dtype="int16"))
 
 
 class WavStreamReceiver(StreamReceiver):
@@ -144,3 +217,35 @@ class WavStreamReceiver(StreamReceiver):
             if len(data) == 0:
                 break
             yield data
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    receiver = VBANStreamingReceiver(
+        "127.0.0.1", "Stream1", 6990, current_sample_rate=44100, current_channels=1, chunk_size=512
+    )
+
+    send_queue = queue.Queue()
+    sender = VBANStreamingSender("192.168.1.206", "Stream2", 6980, data_queue=send_queue, sample_rate=44100, channels=1)
+    import threading
+
+    # while True:
+    #     chunk = receiver.recv_once()
+    #     print(len(chunk))
+    #     data = np.frombuffer(chunk, dtype="int16")
+    #     # data = data / 32768.0
+    #     print(len(data))
+    #     sender.send_once(data)
+    # i = 0
+
+    # i += 1
+    # if i == 10:
+    #     break
+    # send_queue.put(None)
+
+    # sender.send_thread()
+
+    th = threading.Thread(target=sender.send_thread)
+    th.start()
+    for chunk in receiver.recv_generator():
+        send_queue.put(chunk)

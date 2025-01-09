1abf0b3286edddc5c3dd542c1e66cc13b9f83dc2
Otatsu0319 <91446491+Otatsu0319@users.noreply.github.com>
2025-01-09 07:09:03 +0000

    翻訳モデルの調整

--- voice_transcriber.py
+++ voice_transcriber.py
@@ -1,6 +1,10 @@
 import multiprocessing as mp
+import os
+import time
 
 import numpy as np
+import torch
+from transformers import pipeline
 
 # from llama_cpp import Llama
 from gemma2_trt import Gemma2
@@ -14,23 +18,33 @@ from whisper_trt import WhisperTRTLLM
 MODEL_LABEL = "large-v3"  # or "distil-large-v3"
 
 PROMPT_JP2EN = "Translate this from Japanese to English:\nJapanese: {0}"
-PROMPT_EN2JP = "Translate this from English to Japanese:\nEnglish: {0}"
+# PROMPT_EN2JP = "Translate this from English to Japanese:\nEnglish: {0}"
+PROMPT_EN2JP = "次の発言を英語から日本語に翻訳してください:\n\n{0}"
 "/workspace/streaming_translate_application/models/gemma-2-2b-jpn-it"
 
 mp.set_start_method('spawn', force=True)
 
 
 def translate_process(queue):
-    translator = Gemma2("/workspace/streaming_translate_application/models/gemma-2-2b-jpn-it_bf16")
+    # translator = Gemma2("/workspace/streaming_translate_application/models/gemma-2-2b-jpn-it_bf16")
+    # translator = pipeline('translation', model='staka/fugumt-en-ja', device="cuda")
+    translator = pipeline(
+        "text-generation",
+        model="google/gemma-2-2b-it",
+        model_kwargs={"torch_dtype": torch.bfloat16},
+        device="cuda",  # replace with "mps" to run on a Mac device
+    )
 
     while True:
         segment = queue.get()
         if segment is None:
             break
         # print("[id:%d, p:%.02f] %s" % (segment.id, segment.avg_logprob, segment.text))
-        output = translator(PROMPT_EN2JP.format(segment))
+        st = time.time()
+        output = translator(PROMPT_EN2JP.format(segment), return_full_text=False, max_new_tokens=1024)
+        # output = translator(segment)
         # print("translated: ", output['choices'][0]['text'])
-        print("translated: ", output)
+        print(f"time: {time.time() - st} translated: {output}")
 
 
 class VoiceTranscriber:
@@ -61,13 +75,28 @@ class VoiceTranscriber:
                     self.p.join()
                 break
             speech = np.array(speech)
-
+            st = time.time()
             segments, probs = self.whisper_model.transcribe(
                 speech, language="ja", log_prob_threshold=self.transcribe_log_probability_threshold
             )
+            print("transcribe time: ", time.time() - st)
             for i in range(len(segments)):
 
                 if self.translate:
                     self.text_queue.put(segments[i])
                 else:
                     print("[id:%d, p:%.02f] %s" % (i, probs[i], segments[i]))
+
+
+if __name__ == "__main__":
+
+    text_queue = mp.Queue()
+    p = mp.Process(target=translate_process, args=(text_queue,))
+    p.start()
+
+    text = open(os.path.join(os.path.dirname(__file__), "assets/sample_en.txt"), "r").read().split("\n")
+
+    for i in range(len(text)):
+        text_queue.put(text[i])
+    text_queue.put(None)
+    p.join()
